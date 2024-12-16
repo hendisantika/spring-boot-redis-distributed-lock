d802d0ed34976201da2599388df09fb9665378f8
hendisantika <hendisantika@yahoo.co.id>
2024-12-16 11:06:51 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/redisdistributedlock/SpringBootRedisDistributedLockApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.redisdistributedlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisDistributedLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisDistributedLockApplication.class, args);
+    }
+
+}
