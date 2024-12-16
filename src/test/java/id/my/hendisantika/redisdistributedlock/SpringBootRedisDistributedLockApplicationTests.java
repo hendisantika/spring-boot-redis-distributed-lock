9f9a3710c527fff5bc6bf298294b29f35a8f6099
hendisantika <hendisantika@yahoo.co.id>
2024-12-16 11:31:09 +0700

    Modify Test🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/redisdistributedlock/SpringBootRedisDistributedLockApplicationTests.java
+++ src/test/java/id/my/hendisantika/redisdistributedlock/SpringBootRedisDistributedLockApplicationTests.java
@@ -1,13 +1,44 @@
 package id.my.hendisantika.redisdistributedlock;
 
-import org.junit.jupiter.api.Test;
+import id.my.hendisantika.redisdistributedlock.repository.ProductRepository;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootRedisDistributedLockApplicationTests {
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17-alpine3.20");
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
     }
 
 }
