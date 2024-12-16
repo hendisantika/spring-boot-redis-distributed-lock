962634413d42c2aebe5d1d446eab455ada65e449
hendisantika <hendisantika@yahoo.co.id>
2024-12-16 11:11:06 +0700

    Add data.sql file🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO product (id, name, stock, updated_by)
+SELECT 1,
+       'Iphone',
+       3,
+       'admin' WHERE NOT EXISTS (SELECT 1 FROM PRODUCT WHERE id = 1);
+
+INSERT INTO product (id, name, stock, updated_by)
+SELECT 2,
+       'Macbook',
+       10,
+       'admin' WHERE NOT EXISTS (SELECT 1 FROM PRODUCT WHERE id = 2);
